fc7faf576786c226c59cb2f95c8d3dc41a3462db
Your Name <you@example.com>
2022-05-22 01:27:49 +0300

    feat: complete 2/6 tasks

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -3,17 +3,17 @@ const path = require("path");
 const readline = require("readline");
 const { stdin: input, stdout: output } = require("process");
 
-let filePath = path.join(__dirname, "text.txt");
-let stream = fs.createWriteStream(filePath);
-
+const filePath = path.join(__dirname, "text.txt");
+const stream = fs.createWriteStream(filePath);
 const rl = readline.createInterface({ input, output });
 
-const handler = (answer) => {
-    stream.write(`${answer} \n`);
-    stream.end();
-    rl.close();
-};
+rl.write("Please enter any text! (for exit enter 'exit' or use exit hotkey)\n");
 
-rl.question("Hello! Type anything text! \n", handler);
+rl.on("line", (text) => {
+    if (text === "exit") process.exit();
+    stream.write(`${text}\n`);
+});
 
-//process.on("exit", () => {});
+process.on("exit", (code) => {
+    if (code === 0) console.log("Process exit, the text has been written to a file");
+});
