34e8349db34e3e4e6f4d107b16de420cf2708005
Ryabaa <you@example.com>
2022-05-23 18:58:41 +0300

    feat: complete 4/6 tasks

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,23 @@
+const { mkdir, readdir, copyFile } = require("fs/promises");
+const { unlink } = require("fs");
+const path = require("path");
 
+const srcPath = path.join(__dirname, "files");
+const distPath = path.join(__dirname, "files-copy");
+
+mkdir(distPath, { recursive: true });
+
+readdir(distPath, { withFileTypes: true }).then((files) => {
+    files.forEach((file) => {
+        const filePath = path.join(distPath, file.name);
+        unlink(filePath, () => {});
+    });
+});
+
+readdir(srcPath, { withFileTypes: true }).then((files) => {
+    files.forEach((file) => {
+        const srcFilePath = path.join(srcPath, file.name);
+        const distFilePath = path.join(distPath, file.name);
+        copyFile(srcFilePath, distFilePath);
+    });
+});
